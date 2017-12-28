2f6b3db27150bd54704008436a524f637a8ba4f1
Manjunath Bettadapura <b.manjunath.n@gmail.com>
2017-12-28 11:53:35 -0600

    Added many to one association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,6 @@
 class Stock < ApplicationRecord
 	has_many :user_stocks
-	has_many :stocks, through: :user_stocks
+	has_many :users, through: :user_stocks
 	
 	def self.new_from_lookup(ticker_symbol)
 		begin
