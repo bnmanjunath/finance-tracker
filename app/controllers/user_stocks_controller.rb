1505a2fc75b31f37b25da2197aa1f60fa0ba3dac
Manjunath Bettadapura <b.manjunath.n@gmail.com>
2017-12-28 15:09:37 -0600

    Add stock deletion

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -6,14 +6,15 @@ class UserStocksController < ApplicationController
 			stock.save
 		end
 		@user_stock = UserStock.create(user: current_user, stock: stock)
-<<<<<<< HEAD
-		flash[:success] = "#{@user_stock.stock.name} has been successfully added to your portfolio"
-		redirect_to my_portfolio_path
-	end
-end
-=======
 		flash[:success] = "Stock #{@user_stock.stock.name} was successfully added to portfolio"
 		redirect_to my_portfolio_path
 	end
+
+	def destroy
+		stock = Stock.find(params[:id])
+		@user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+		@user_stock.destroy
+		flash[:notice] = "Stock was successfully removed from portfolio"
+		redirect_to my_portfolio_path
+	end
 end
->>>>>>> production
